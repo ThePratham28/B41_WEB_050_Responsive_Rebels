7c05e61e414e3fad37b7b1be0d9a42a7032ecaca
ThePratham28 <prathamchopde31@gmail.com>
2024-12-18 19:34:38 +0530

    login/siginup api

---
+++ backend/src/api/routes/auth.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { signupUser, loginUser } from "../controllers/auth.controller.js";
+
+const authRouter = Router();
+
+authRouter.post("/signup", signupUser);
+authRouter.post("/login", loginUser);
+
+export default authRouter;
